220fd313d5fed158d95f06a9242448470303a111
jp-rodrigues <69464669+jp-rodrigues@users.noreply.github.com>
2022-04-18 20:47:01 -0300

    Update prometheus_install.sh
    update

--- scripts/prometheus_install.sh
+++ scripts/prometheus_install.sh
@@ -56,10 +56,10 @@ After=network-online.target
 User=prometheus
 Group=prometheus
 Type=simple
-ExecStart=/usr/local/bin/prometheus \
-    --config.file /etc/prometheus/prometheus.yml \
-    --storage.tsdb.path /var/lib/prometheus/ \
-    --web.console.templates=/etc/prometheus/consoles \
+ExecStart=/usr/local/bin/prometheus\
+    --config.file /etc/prometheus/prometheus.yml\
+    --storage.tsdb.path /var/lib/prometheus/\
+    --web.console.templates=/etc/prometheus/consoles\
     --web.console.libraries=/etc/prometheus/console_libraries
 
 [Install]
