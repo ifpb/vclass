a2e5c9f05dc1be0400e9e71287a3aefb7a946817
jp-rodrigues <69464669+jp-rodrigues@users.noreply.github.com>
2022-04-18 18:26:43 -0300

    Update prometheus_install.sh
    modificações, incluindo as configurações do prometheus no script

--- scripts/prometheus_install.sh
+++ scripts/prometheus_install.sh
@@ -18,6 +18,48 @@ cp $version/prometheus /usr/local/bin/;
 
 cp $version/promtool /usr/local/bin/;
 
-chown -R prometheus:prometheus /etc/prometheus/consoles
+chown -R prometheus:prometheus /etc/prometheus/consoles;
 
-chown -R prometheus:prometheus /etc/prometheus/consoles_libraries
+chown -R prometheus:prometheus /etc/prometheus/consoles_libraries;
+
+mv /etc/prometheus/prometheus.yml /etc/prometheus/config-backup; 
+
+touch /etc/prometheus/prometheus.yml;
+
+tee -a /etc/prometheus/prometheus.yml  << EOF
+
+global:
+  scrape_interval:     10s
+  evaluation_interval: 20s
+rule_files:
+  - 'alert.rules'
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+         - targets: ['localhost:9090']
+EOF;
+
+tee -a /etc/systemd/system/prometheus.service << EOF 
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOF;
+systemctl daemon-reload;
+systemctl start prometheus;
+systemctl enable prometheus;
+systemctl status prometheus;
