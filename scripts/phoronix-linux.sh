b3c1739a6f81834f5dfb66f6c08536fc032ab420
victorpjp <65964736+victorpjp@users.noreply.github.com>
2022-05-01 22:51:21 -0300

    Update phoronix-linux.sh

--- scripts/phoronix-linux.sh
+++ scripts/phoronix-linux.sh
@@ -6,3 +6,7 @@ wget  https://phoronix-test-suite.com/releases/$version.tar.gz
 tar xvf  $version.tar.gz;
 cd  phoronix-test-suite;
 sudo ./install-sh;
+#Abaixo os testes benchmark instalados no projeto vclass
+sudo  phoronix-test-suite force-install benchmark iozone;
+sudo  phoronix-test-suite  force-install t-test1;
+sudo phoronix-test-suite force-install iozone;
