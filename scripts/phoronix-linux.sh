e5dcb9bb7dec6a04520b43653c7452979b60d09f
jp-rodrigues <69464669+jp-rodrigues@users.noreply.github.com>
2022-08-21 09:02:03 -0300

    Update phoronix-linux.sh

--- scripts/phoronix-linux.sh
+++ scripts/phoronix-linux.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 version='phoronix-test-suite-10.8.3';
+
 apt update && apt upgrade;
+
 apt install -y    php php-xml  php-cli php-gd;
-wget  https://phoronix-test-suite.com/releases/$version.tar.gz
+
+wget  https://phoronix-test-suite.com/releases/$version.tar.gz;
 tar xvf  $version.tar.gz;
+
 cd  phoronix-test-suite;
+
 sudo ./install-sh;
+
 #Abaixo os testes benchmark phoronix instalados no projeto vclass
 sudo  phoronix-test-suite force-install benchmark iozone;
+
 sudo  phoronix-test-suite  force-install t-test1;
-sudo phoronix-test-suite force-install iozone;
+
+sudo phoronix-test-suite force-install blender;
